5b13a96b0892757fa0c00cb8499bb652b7a1b762
pubestpubest <pubestpubest@gmail.com>
2023-09-28 15:17:20 +0700

    finished

--- src/app/api/course/search/route.js
+++ src/app/api/course/search/route.js
@@ -6,7 +6,10 @@ export const GET = async (request) => {
   const prisma = getPrisma();
 
   //Modify following line so that it find course with "searchText" variable
-  const courses = await prisma.course.findMany({});
+  const courses = await prisma.course.findMany({
+    where: { title: { contains: searchText, mode: "insensitive" } },
+    orderBy: { courseNo: "asc" },
+  });
 
   return NextResponse.json({ ok: true, courses });
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -91,8 +91,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="650610798"
+                fullName="Pubest Ruengkum"
                 year="2023"
               />
             </Container>
